70a83dc643ef7828a4f59df48a033e742e49fb70
Aryan Jain <aryanjain987@gmail.com>
2025-04-21 22:27:26 +0800

    cleanup

--- src/RustEvaluator.ts
+++ src/RustEvaluator.ts
@@ -560,46 +560,6 @@ class RustEvaluatorVisitor
     return { type: "number", val: 40 };
   }
 
-  // Visit a parse tree produced by SimpleLangParser#statement
-  // visitStatement(ctx: StatementContext): number {
-  //     console.log("hi aryan")
-  //     console.log(ctx)
-  //     return 42;
-  // }
-
-  // Visit a parse tree produced by SimpleLangParser#expression
-  // visitExpression(ctx: ExpressionContext): number {
-  //     if (ctx.getChildCount() === 1) {
-  //         // INT case
-  //         return parseInt(ctx.getText());
-  //     } else if (ctx.getChildCount() === 3) {
-  //         if (ctx.getChild(0).getText() === '(' && ctx.getChild(2).getText() === ')') {
-  //             // Parenthesized expression
-  //             return this.visit(ctx.getChild(1) as ExpressionContext);
-  //         } else {
-  //             // Binary operation
-  //             const left = this.visit(ctx.getChild(0) as ExpressionContext);
-  //             const op = ctx.getChild(1).getText();
-  //             const right = this.visit(ctx.getChild(2) as ExpressionContext);
-
-  //             switch (op) {
-  //                 case '+': return left + right;
-  //                 case '-': return left - right;
-  //                 case '*': return left * right;
-  //                 case '/':
-  //                     if (right === 0) {
-  //                         throw new Error("Division by zero");
-  //                     }
-  //                     return left / right;
-  //                 default:
-  //                     throw new Error(`Unknown operator: ${op}`);
-  //             }
-  //         }
-  //     }
-
-  //     throw new Error(`Invalid expression: ${ctx.getText()}`);
-  // }
-
   // Override the default result method from AbstractParseTreeVisitor
   protected defaultResult(): number {
     return 0;
